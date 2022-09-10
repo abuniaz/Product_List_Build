75c3ad0d126a66c7a6d3f1924a11d3507110dc89
Abu Niaz <niaz35-507@diu.edu.bd>
2022-09-10 12:07:00 +0600

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListBuild extends StatefulWidget {
+  const ListBuild({Key? key}) : super(key: key);
+
+  @override
+  State<ListBuild> createState() => _ListBuildState();
+}
+
+class _ListBuildState extends State<ListBuild> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
